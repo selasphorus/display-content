375b92aa8029b58f1ad87ea0e0631dccf0538447
atc <birdhive@gmail.com>
2023-02-17 12:29:04 -0500

    Hide TS post info

--- display-content.php
+++ display-content.php
@@ -992,7 +992,7 @@ function birdhive_display_collection ( $a = array() ) {
 			} else if ( isset($item['post_object']) ) {
 				$post = $item['post_object'][0];
 			}
-			$ts_info .= 'post: <pre>'.print_r($post, true).'</pre>'; // tft
+			//$ts_info .= 'post: <pre>'.print_r($post, true).'</pre>'; // tft
 			$post_type = $post->post_type;
 			
 			//$ts_info .= 'item: <pre>'.print_r($item, true).'</pre>'; // tft
