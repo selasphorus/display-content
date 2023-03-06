f163461f9a829f0276b273f93ad0a7454e448586
atc <birdhive@gmail.com>
2023-03-06 09:30:32 -0500

    cont.

--- display-content.php
+++ display-content.php
@@ -910,9 +910,10 @@ function display_grid_item ( $item = array(), $display_atts = array(), $ts_info 
 	if ( isset($display_atts['aspect_ratio']) ) { $aspect_ratio = $display_atts['aspect_ratio']; } else { $aspect_ratio = "square"; }
 	
 	// Begin building item_info
-	if ( $aspect_ratio == "square" ) { 
-		$item_title = "<h3>".$item_title."</h3>";
-		if ( !empty($item_subtitle) ) { $item_subtitle = "<h4>".$item_subtitle."</h4>"; }
+	if ( $aspect_ratio == "square" ) {
+		$hclass = "";
+		if ( !empty($item_subtitle) ) { $hclass = "with-subtitle"; $item_subtitle = "<h4>".$item_subtitle."</h4>"; }
+		$item_title = "<h3 $hclass>".$item_title."</h3>";
 	}
 	$item_info = $item_title;
 	
