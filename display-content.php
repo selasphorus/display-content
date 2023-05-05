76f71cc73331e5698f183dcf1bb78bebcefcddcd
atc <birdhive@gmail.com>
2023-05-05 13:51:39 -0400

    cont.

--- display-content.php
+++ display-content.php
@@ -916,10 +916,10 @@ function birdhive_display_collection ( $a = array() ) {
 				}*/
 				$img_size = array( 250, 250); // tft
 				//$item_ts_info .= "<!-- Attempting to retrieve image_id using sdg_post_thumbnail fcn...-->";
-				$tmp = sdg_post_thumbnail ( $post_id, $img_size, true, false );
-				//$image_id = sdg_post_thumbnail ( $post_id, $img_size, true, false, "id" ); //sdg_post_thumbnail ( $post_id, $img_size, $use_custom_thumb, $echo, $return )
+				//$tmp = sdg_post_thumbnail ( $post_id, $img_size, true, false );
+				$image_id = sdg_post_thumbnail ( $post_id, $img_size, true, false, "id" ); //sdg_post_thumbnail ( $post_id, $img_size, $use_custom_thumb, $echo, $return )
 				//$item_ts_info .= '<!-- sdg_post_thumbnail post_id: '.$post_id.'; image_id: '.$image_id.'; custom thumb true, echo false -->'; // tft
-				$item_ts_info .= '<!-- sdg_post_thumbnail tmp: '.$tmp.' -->'; // tft
+				//$item_ts_info .= '<!-- sdg_post_thumbnail tmp: '.$tmp.' -->'; // tft
 				
 				/*
 				// First, check to see if the post has a Custom Thumbnail
