1719899545c073abd2a088a7fb088b47d38692f5
atc <birdhive@gmail.com>
2023-05-18 12:14:29 -0400

    Added custom sizes for grid images (rather than do it through Image Regenerate plugin)

--- display-content.php
+++ display-content.php
@@ -48,6 +48,18 @@ $plugin_path = plugin_dir_path( __FILE__ );
 $posttypes_filepath = $plugin_path . 'inc/posttypes.php';
 if ( file_exists($posttypes_filepath) ) { include_once( $posttypes_filepath ); } else { echo "no $posttypes_filepath found"; }
 
+// Add custom image sizes
+// TODO: build in option to customize dimensions per site
+add_image_size( 'grid_crop_square', 600, 600, true );
+add_image_size( 'grid_crop_rectangle', 534, 300, true );
+add_filter( 'image_size_names_choose', 'birdhive_custom_image_sizes' );
+function birdhive_custom_image_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'grid_crop_square' => __( 'Grid Crop (square)' ),
+        'grid_crop_rectangle' => __( 'Grid Crop (rectangle)' ),
+    ) );
+}
+
 // Enqueue scripts and styles -- WIP
 add_action( 'wp_enqueue_scripts', 'dc_scripts_method' );
 function dc_scripts_method() {
