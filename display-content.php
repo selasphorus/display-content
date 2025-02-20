c8c7dd6bdb2d5a825841d812d850a775e3e04d00
atc <birdhive@gmail.com>
2025-02-20 13:18:46 -0500

    cont.

--- display-content.php
+++ display-content.php
@@ -1337,7 +1337,7 @@ function build_item_arr ( $item, $arr_styling = array() ) { // TODO: come up wit
 			if ( $event_start_datetime ) {
 				if ( $event_all_day ) {
 					if ( $event_end_datetime ) {
-						$item_date_str = date_i18n( "l, F j", strtotime($event_start_datetime) );
+						$item_date_str = date_i18n( "F j", strtotime($event_start_datetime) );
 					} else {
 						$item_date_str = date_i18n( "l, F j, Y", strtotime($event_start_datetime) );
 					}
@@ -1349,7 +1349,7 @@ function build_item_arr ( $item, $arr_styling = array() ) { // TODO: come up wit
 			}
 			// Multi-day event? Then show the end date
 			if ( $event_end_datetime && $event_all_day ) {
-				$item_date_str .= "  to ".date_i18n( "l, F j, Y", strtotime($event_end_datetime) );
+				$item_date_str .= "  to ".date_i18n( "F j, Y", strtotime($event_end_datetime) );
 			}
 			$item_date_str = str_replace(array('am','pm'),array('a.m.','p.m.'),$item_date_str); // STC >> TODO: generalize formatting options
 			
