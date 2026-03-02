5b21e16736510a859b073b3790fb9b560172cdd5
atc-mba25 <birdhive@gmail.com>
2026-03-02 14:42:21 -0500

    catchup

--- inc/posttypes.php
+++ inc/posttypes.php
@@ -1,5 +1,4 @@
 <?php
-
 defined( 'ABSPATH' ) or die( 'Nope!' );
 
 // Make sure we don't expose any info if called directly
@@ -7,7 +6,6 @@ if ( !function_exists( 'add_action' ) ) {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
 }
-
 
 /*** CONTENT COLLECTIONS ***/
 
